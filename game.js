69b09034d0da7a065998b26dceb739493e39fec6
맹주영 <mjy9088@naver.com>
2019-04-27 19:32:20 +0900

    테스트

--- game.js
+++ game.js
@@ -1,11 +1,37 @@
 var data;
 
+var _w, _h;
+
 function onStartGame(w, h, m)
 {
+	_w = w;
+	_h = h;
 }
 
 function onClickTile(x, y)
 {
-	tds[y][x].classList.toggle("known");
+	if(x < 0 || y < 0 || x >= _w || y >= _h)
+	{
+		return;
+	}
+	if(tds[y][x].classList.contains("known"))
+	{
+		return;
+	}
+	tds[y][x].classList.add("known");
+	ff(x, y);
+}
+
+var dir8 =  [[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]];
+
+function ff(x, y)
+{
+	if(tds[y][x].classList.contains("n0"))
+	{
+		for(var i = 0; i < 8; i++)
+		{
+			onClickTile(x + dir8[i][0], y + dir8[i][1]);
+		}
+	}
 }
 
